e91f1acf4be3c12dfd71785f46ef0037882b5dc9
Yury Yurochko <yurochkoyura@gmail.com>
2024-02-16 17:39:04 +0700

    feat(project): step 2 - basic ansible setup (#2)
    * feat(project): step 2 - basic ansible setup
    
    * feat(project): step 2 - basic ansible setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/forward32/devops-for-programmers-project-77/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/forward32/devops-for-programmers-project-77/actions)
+
+## Terraform
+В папке terraform лежит все необходое для поднятия инфраструктуры с помощью terraform. В качестве облачного провайдера используется DigitalOcean, сайт доступен по https://forward32.online
+
+## Ansible
+В папке ansible лежит все необходимое для настройки окружения и разворачивания Redmine через ansible.
+
+## Makefile
+Для удобства использования создан Makefile со следующими таргетами:
+...
--- terraform/droplet.tf
+++ terraform/droplet.tf
@@ -1,51 +1,19 @@
 resource "digitalocean_droplet" "hexlet-1" {
-  image  = "ubuntu-22-04-x64"
+  image  = "docker-20-04"
   name   = "hexlet-1"
   region = "sgp1"
   size   = "s-1vcpu-1gb"
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
-
-  connection {
-    host        = self.ipv4_address
-    user        = "root"
-    type        = "ssh"
-    private_key = file(var.pvt_key)
-    timeout     = "2m"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      "sudo apt update",
-      "sudo apt install -y nginx"
-    ]
-  }
 }
 
 resource "digitalocean_droplet" "hexlet-2" {
-  image  = "ubuntu-22-04-x64"
+  image  = "docker-20-04"
   name   = "hexlet-2"
   region = "sgp1"
   size   = "s-1vcpu-1gb"
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
-
-  connection {
-    host        = self.ipv4_address
-    user        = "root"
-    type        = "ssh"
-    private_key = file(var.pvt_key)
-    timeout     = "2m"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      "sudo apt update",
-      "sudo apt install -y nginx"
-    ]
-  }
 }
